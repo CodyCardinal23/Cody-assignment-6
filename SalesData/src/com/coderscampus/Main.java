9e89029c010c118d9eb233a20a49f6a498f6839d
CodyCardinal23 <codycardinal@gmail.com>
2024-07-22 17:18:10 -0600

    output

--- SalesData/src/com/coderscampus/Main.java
+++ SalesData/src/com/coderscampus/Main.java
@@ -2,6 +2,55 @@ package com.coderscampus;
 
 public class Main {
     public static void main(String[] args) {
-         System.out.println("First commit");
+
+
+//        In this assignment you will be analyzing sales data for the sales of Tesla vehicles between 2016 and 2019.
+//
+//        Your job will be to read the data from 3 different CSV files and store that information for analysis and reporting.
+//
+//                You will need to use Java Streams to go through this data and output the following information onto the console:
+//
+//        Note: anywhere you see #### or yyyy-MM, you'll need to output actual numbers / dates based on your analysis.
+
+
+        // Model 3 Yearly Sales Report
+//        2017 -> 1,770
+//        2018 -> 139,782
+//        2019 -> 158,925
+//
+//        The best month for Model 3 was: 2019-12
+//        The worst month for Model 3 was: 2017-07
+
+
+        // Model S Yearly Sales Report
+//        2016 -> 26,321
+//        2017 -> 28,260
+//        2018 -> 25,945
+//        2019 -> 14,100
+//
+//        The best month for Model S was: 2016-12
+//        The worst month for Model S was: 2019-02
+
+
+        // Model X Yearly Sales Report
+//        2016 -> 18,223
+//        2017 -> 21,515
+//        2018 -> 29,000
+//        2019 -> 21,925
+//
+//        The best month for Model X was: 2018-12
+//        The worst month for Model X was: 2016-01 and 2016-02
+
+
+//        Hints
+//
+//        You don't need to output this entire report in a single stream.
+//
+//        You'll want to read the data into three separate Lists.
+//
+//        You will likely need to stream through each of the Lists a few times to be able to output all the necessary data in the report.
+//
+//        Remember that grouping data is an important thing to do.
+
     }
 }
